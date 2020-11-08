859ab951d4a99d609baaa4f8d5ab6c1af4729533
Joe Bigio <jvbigio@gmail.com>
2020-11-08 00:47:34 -0500

    working on using async/await for api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-// import axios from 'axios'
+import axios from 'axios'
 
 class App extends React.Component {
   constructor () {
@@ -12,24 +12,29 @@ class App extends React.Component {
   }
 
   // one version:
-  componentDidMount () {
+  async componentDidMount () {
     this.setState({ loading: true })
-    fetch('https://swapi.dev/api/people/1')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          loading: false,
-          character: data
-        })
-      })
+    const response = await axios.get('https://swapi.dev/api/people/1')
+    // const json = await response.json()
+    // const data = response.data.data <-- equivalent to:
+    const { data } = response.data
+    // const { data } = response.json
+    // { character } = response.json
+    this.setState({
+      loading: false,
+      character: data
+    })
+    console.log(response.data.name) // Luke Skywalker
   }
 
   render () {
-    const text = this.state.loading ? 'loading...' : this.state.character.name
+    // console.log(this.state.character);
+    // this.state.character.map(elem => console.log(elem))
+    // const text = this.state.loading ? 'loading...' : this.state.character
     return (
       <div className='App'>
         <h1>Star Wars API Project</h1>
-        <p>{text}</p>
+        {/* <p>{text}</p> */}
       </div>
     )
   }
