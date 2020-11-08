fc5e99b4622fa893a2ea2e8f86180eb3fd4de2e5
Joe Bigio <jvbigio@gmail.com>
2020-11-07 23:26:01 -0500

    fetch api works...will test async/await

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import React from 'react'
 import './App.css'
-import axios from 'axios'
+// import axios from 'axios'
 
-function App () {
-  return (
-    <div className='App'>
-      <h1>Star Wars API Project</h1>
-    </div>
-  )
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      loading: false,
+      character: {}
+    }
+  }
+
+  // one version:
+  componentDidMount () {
+    this.setState({ loading: true })
+    fetch('https://swapi.dev/api/people/1')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          character: data
+        })
+      })
+  }
+
+  render () {
+    const text = this.state.loading ? 'loading...' : this.state.character.name
+    return (
+      <div className='App'>
+        <h1>Star Wars API Project</h1>
+        <p>{text}</p>
+      </div>
+    )
+  }
 }
 
 export default App
