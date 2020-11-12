14d887f5b5764b8cd58b87a074faccd88ed4cb67
Joe Bigio <jvbigio@gmail.com>
2020-11-11 23:11:29 -0500

    changed header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header () {
   return (
-    <div className='nav'>
+    <div className='nav fixed-top'>
       <div className='logo'>
         <span><i className='fas fa-jedi' /></span>Star Wars <img className='vader-logo' src='https://i.postimg.cc/B6MRV4jG/vader.png' alt='darth vader' />
       </div>
