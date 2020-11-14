82bde22dbb6e8f99ac5ef793311b73b753213248
Joe Bigio <jvbigio@gmail.com>
2020-11-14 13:45:16 -0500

    refactored UsePagination

--- src/components/UsePagination.js
+++ src/components/UsePagination.js
@@ -6,9 +6,9 @@ const UsePagination = (props) => {
   const { loading } = props
 
   const active = 1
-  const itemsPerPage = []
+  const items = []
   for (let number = 1; number <= 10; number++) {
-    itemsPerPage.push(
+    items.push(
       <Pagination.Item key={number} active={number === active}>
         {number}
       </Pagination.Item>
@@ -21,7 +21,7 @@ const UsePagination = (props) => {
   return (
     <Pagination className='pagination'>
       <Pagination.Prev />
-      {itemsPerPage}
+      {items}
       <Pagination.Next />
     </Pagination>
   )
