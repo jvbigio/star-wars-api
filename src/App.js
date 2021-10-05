62b16cabf23d097da1393a2d523ddb1180b2701b
Joe Bigio <jvbigio@gmail.com>
2021-10-05 05:31:55 -0400

    refactored get rqsts for planet and species. https replace no longer needed

--- src/App.js
+++ src/App.js
@@ -31,13 +31,12 @@ const App = () => {
 
   const processCharacterData = async (characters) => {
     for (const character of characters) {
-      const planetURL = character.homeworld.replace('http', 'https')
-      const planet = await axios.get(planetURL)
+      const planet = await axios.get(character.homeworld)
 
       character.homeworld = planet.data.name
 
-      const speciesURL = character.species.toString().replace('http', 'https')
-      const species = await axios.get(speciesURL)
+      const species = await axios.get(character.species)
+
       !species.data.name ? character.species = 'Human' : character.species = species.data.name
     }
     return characters
